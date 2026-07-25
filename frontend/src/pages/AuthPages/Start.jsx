139dc62a169b25df58f69988b522d7b891346a28
Dev-Farrukh <m.farrukhnoman@gmail.com>
2026-07-25 13:52:28 +0000

    Implemented start page

---
+++ frontend/src/pages/AuthPages/Start.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom"
+
+const Start = () => {
+  return (
+    <main className="min-h-dvh flex flex-col items-center justify-center px-6 pt-10 text-center gap-8 md:gap-10 lg:gap-10">
+
+    {/* Ride Image */}
+    <img
+        src="src/assets/images/ride.png"
+        alt="Ride"
+        className="w-64 sm:w-72 md:w-80 lg:w-96 object-contain"
+    />
+
+    <div className="max-w-2xl">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-[#1f1e1d]">
+            Book Your Ride
+        </h1>
+
+        <p className="mt-4 text-base sm:text-lg md:text-xl text-gray-600 leading-relaxed">
+            Get where you need to go quickly and comfortably.
+            Connect with trusted riders and enjoy a smooth,
+            hassle-free journey every time.
+        </p>
+    </div>
+
+    {/* Go Button */}
+    <div className="rounded-full border-2 border-[#edaf10] p-2">
+        <Link
+            to="/login"
+            className="inline-block rounded-full bg-[#edaf10] px-6.5 py-6
+             md:px-7 md:py-7 text-lg md:text-xl font-semibold text-white
+            transition-all duration-300 ease-in-out outline-none
+            hover:scale-110 hover:bg-[#dea922]
+            active:scale-95
+            shadow-md hover:shadow-xl"
+        >
+            Go
+        </Link>
+    </div>
+
+</main>
+  )
+}
+
+export default Start
--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import UserLogin from "../pages/AuthPages/UserLogin"
 import UserRegister from "../pages/AuthPages/UserRegister"
+import Start from "../pages/AuthPages/Start"
 
 const Router = () => {
 	return (
@@ -9,6 +10,7 @@ const Router = () => {
             <Route path="/login" element={<UserLogin />} />
             <Route path="/register" element={<UserRegister  />} />
             <Route path="/rider-register" element={<UserRegister  />} />
+            <Route path="/start" element={<Start  />} />
         </Routes>
 	)
 }
