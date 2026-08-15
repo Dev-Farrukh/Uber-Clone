5fd9585fc0feca6dc6057fb076df98596594d7f9
Dev-Farrukh <m.farrukhnoman@gmail.com>
2026-08-15 16:51:41 +0000

    Refactor README and add ride detail service functions

---
+++ backend/src/services/getRideDetail.js
@@ -0,0 +1,5 @@
+export const getCoordinates = async (query) => {}
+
+export const getTime = async (query) => {}
+export const getDistance = async (query) => {}
+export const getSuggestion = async (query) => {}
