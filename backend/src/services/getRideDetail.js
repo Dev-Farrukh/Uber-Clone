35019abe2d9cecf3b24662bc5bd2776415860026
Dev-Farrukh <m.farrukhnoman@gmail.com>
2026-08-16 17:27:51 +0000

    Ongoing changes

--- backend/src/services/getRideDetail.js
+++ backend/src/services/getRideDetail.js
@@ -1,4 +1,6 @@
-export const getCoordinates = async () => {}
+export const getCoordinates = async (address) => {
+    const url = `https://us1.locationiq.com/v1/search?key=YOUR_API_KEY&q=Statue%20of%20Liberty,%20New%20York&format=json`
+}
 
 export const getTime = async () => {}
 export const getDistance = async () => {}
