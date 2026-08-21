2fa8ec13abb6273df463daa94394ffea49f1b49e
Dev-Farrukh <m.farrukhnoman@gmail.com>
2026-08-22 03:56:20 +0500

    explicitly mentioned origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,12 +12,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin : ["https://probable-space-fishstick-g4j5vjr6wjqx2vwg-5173.app.github.dev" , "http://localhost:5173" , "https://ld5859dw-5173.inc1.devtunnels.ms" , envVariables.FRONTEND_URL],
+    origin : ["https://ride-booking-webapp.vercel.app" , "http://localhost:5173" , "https://ld5859dw-5173.inc1.devtunnels.ms" , envVariables.FRONTEND_URL],
     credentials : true ,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 
 }))
+
 
 app.use("/api/user" , userRouter)
 app.use("/api/rider" , riderRouter)
